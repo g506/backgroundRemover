88829182c56c8bbc4b4bae711db40f85560a492e
Govind Goel <govindgoel82@gmail.com>
2023-01-02 12:35:45 +0530

    fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,6 @@ export default class Footer extends Component {
       <div>
         <footer className="footer">
           <div className="container">
-            <span className="text-muted">made with <i className="fa fa-heart heart"/> by</span><a href="https://www.serdarbudak.com.tr">Serdar Budak</a>
           </div>
         </footer>
       </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,12 +5,7 @@ export default class Header extends Component {
     return (
       <div>
         <header className="text-white text-center">
-          <img
-            alt="serdar budak"
-            src="https://www.serdarbudak.com.tr/_next/static/images/logoW-9420a2d091ff277f3f95224812d88adf.png"
-            width="80"
-            className="mb-4"
-          />
+         
 
           <h1 className="display-4">Background Removal Tool</h1>
           <p className="lead mb-0">Fast, simple and great performance.</p>
